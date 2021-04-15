a0ac07ae6551f8e9000774e4f87b6c0f75f14922
xiezuoru <xiezuoru@xiezuorudeMacBook-Pro.local>
2021-04-15 19:13:41 +0800

    更新插件
    更新Web管理端

--- 3.设备管理类/Web管理端（remi）/remi-sfarm1.2.py
+++ 3.设备管理类/Web管理端（remi）/remi-sfarm1.2.py
@@ -1,11 +1,12 @@
 # 将该程序的地址(IP地址加8001端口)做成二维码，手机扫描后就可以访问，可以快速查看信息，控制浇水。
-# 支持多个设备，自动生成二维码。
-# 代码编写：谢作如(2021.4.12)
+# 支持多个设备，同时在页面中生成二维码。
+# 代码编写：谢作如(2021.4.13)
 
 import remi.gui as gui
 from remi import start, App
 import siot,time
 import threading
+import qrcode,base64,io,socket
 
 topic_msg_map={}
 i = 0
@@ -14,9 +15,19 @@ project_list= ['sf88','sf89']
 # 设备编号默认先呈现第一个
 id = 0
 
-iot_server = '192.168.3.136' # mqtt服务器地址
+iot_server = '192.168.3.41' # mqtt服务器地址
 iot_user = 'scope'
 iot_pwd = 'scope'
+
+# 获取IP地址
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
 
 # 处理订阅消息
 def on_topic_subscribe(client,userdata,msg):
@@ -30,7 +41,15 @@ def on_topic_read(topic):
         del topic_msg_map[topic]
         return str(result)
 
-siot.init('',iot_server,user=iot_user,password=iot_pwd)
+data='http://' + get_host_ip() + ':8001'
+img = qrcode.make(data)
+out = io.BytesIO()
+img.save(out, 'PNG')
+base64_data=base64.b64encode(out.getvalue())
+s=base64_data.decode()
+data='data:image/jpeg;base64,%s'%s
+
+siot.init('remi',iot_server,user=iot_user,password=iot_pwd)
 siot.connect()
 for item in project_list:
     siot.subscribe(item + '/light',on_topic_subscribe)
@@ -44,12 +63,14 @@ class MyApp(App):
         super(MyApp, self).__init__(*args)
 
     def main(self):
-        container = gui.VBox(width=400, height=240,style={'margin':'0px auto'})
+        container = gui.VBox(width=400, height=340,style={'margin':'0px auto'})
         self.lbl_01 = gui.Label('开源智慧农场监控页面',style={'font-size': '25px'})
         self.dd = gui.DropDown(['设备编号：' + x for x in project_list],width=150,style={'font-size': '12px'})
         self.lbl_1 = gui.Label('环境光照值：等待数据中')
         self.lbl_2 = gui.Label('土壤湿度值：等待数据中')
         self.lbl_9 = gui.Label(' ')
+        self.img_1 = gui.Image(data,width=150, height=150)
+
         
         self.bt = gui.Button('[ 浇水 ]')
 
@@ -66,6 +87,7 @@ class MyApp(App):
         container.append(self.lbl_2)
         container.append(self.bt)
         container.append(self.lbl_9)
+        container.append(self.img_1)
         
         # 开启新的进程处理MQTT消息
         t = threading.Thread(target=self.showmqtt)
